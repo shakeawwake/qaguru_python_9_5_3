7ec8a2b4bc534c6bf6bd15b9feeef9ece1036c1d
kglazova <kglazova@huntflow.ru>
2024-01-13 11:47:14 +0300

    hw allure

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,36 @@
 import pytest
-from selene import browser, config
+
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import browser
+
+from utils import attach
 
 
-@pytest.fixture(scope="function", autouse=True)
-def settings():
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.timeout = 2.0
-    driver_options = webdriver.ChromeOptions()
-    driver_options.add_argument('--headless=new')
-    browser.config.driver_options = driver_options
+@pytest.fixture(scope='function', autouse=True)
+def setup_browser(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {"enableVNC": True, "enableVideo": True},
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options,
+    )
 
-    browser.config.window_width = 1920
+    browser.config.driver = driver
+    browser.config.base_url = "https://demoqa.com"
     browser.config.window_height = 1080
+    browser.config.window_width = 1920
 
-    yield
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
     browser.quit()
--- tests/test_demo_qa.py
+++ tests/test_demo_qa.py
@@ -1,9 +1,16 @@
 from data.user_data import user
 from pages.registration_page import RegistrationPage
+import allure
 
 
 def test_registration():
     registration_page = RegistrationPage()
-    registration_page.open()
-    registration_page.fill_form(user)
-    registration_page.should_registered_user_with(user)
+
+    with allure.step('Открываем тестируемую форму https://demoqa.com/automation-practice-form'):
+        registration_page.open()
+
+    with allure.step('Заполняем форму'):
+        registration_page.fill_form(user)
+
+    with allure.step('Проверяем форму'):
+        registration_page.should_registered_user_with(user)
