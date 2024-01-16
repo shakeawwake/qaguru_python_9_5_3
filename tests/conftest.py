0338515bacbfee1a5a7e103314e4d1b9ab2e5a28
kglazova <kglazova@huntflow.ru>
2024-01-16 17:53:19 +0300

    add parameters

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,16 +3,24 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import browser
-
 from utils import attach
 
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
+
+def seleniod_browser()
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": browser_version,
         "selenoid:options": {"enableVNC": True, "enableVideo": True},
     }
     options.capabilities.update(selenoid_capabilities)
@@ -20,6 +28,7 @@ def setup_browser(request):
         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options,
     )
+
 
     browser.config.driver = driver
     browser.config.base_url = "https://demoqa.com"
